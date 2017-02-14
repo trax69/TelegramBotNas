8706074a4c277337a9b0c3379c3367fc4110e7e6
DoomAngels <mihai-emanuel@hotmail.com>
2017-02-14 19:54:39 +0100

    Listar Argumentos

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace nasBot
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+			foreach (string arg in args) {
+				Console.WriteLine(arg);
+			}
+			Console.ReadLine();
 		}
 	}
 }
