37d44a651a73fabb2a8e663285987c267c1c4604
DoomAngels <mihai-emanuel@hotmail.com>
2017-02-14 18:29:49 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace telegramNasBot
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			foreach (string data in args) {
+				Console.WriteLine(data);
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
