44362764c3b89a0f56b0d9ff23b6d30f3097f6a0
DoomAngels <mihai-emanuel@hotmail.com>
2017-03-06 16:08:53 +0100

    Subiendo la libreria

---
+++ botSettings/botSettings.cs
@@ -0,0 +1,459 @@
+﻿//
+//  MyClass.cs
+//
+//  Author:
+//       Big Cash Monkeys <admin@bcm.ovh>
+//
+//  Copyright (c) 2017 Big Cash Monkeys
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using Newtonsoft.Json;
+
+using botSettings.Checks;
+using botSettings.Enums;
+
+namespace botSettings
+{
+	/// <summary>
+	/// Class used to save Telegram Bot Settings
+	/// </summary>
+	public class botSetting
+	{
+		// Private
+		private bool isConfigSet = false;
+		private bool isPathSet = false;
+		private botChecks chk = new botChecks();
+		private botConfig conf = new botConfig();
+
+		public botSetting(string filePath)
+		{
+			setFilePath (filePath);
+		}
+
+		/// <summary>
+		/// Loads contents of the config file.
+		/// Debugs to Console if Error.
+		/// </summary>
+		/// <returns><c>String</c> to parse as class if config file was loaded, <c>null</c> otherwise.</returns>
+		/// <param name="filePath">Path and Name of the file to be loaded.</param>
+		private botConfig loadConfFile(string filePath)
+		{
+			botConfig data = new botConfig();
+			Debug.WriteLine ("Loading config file.");
+			try {
+
+				if (!File.Exists (filePath))
+				{
+					File.Create (filePath).Close ();	// Create the file for the first time
+					saveConfFile(filePath, dataToJson(data));	// Parse the empty class for the first time and Save It.
+				}
+
+				using (StreamReader sR = new StreamReader(filePath))
+				{
+					data = JsonConvert.DeserializeObject<botConfig>(sR.ReadLine());
+					this.isConfigSet = true;
+				}
+
+			} 
+			catch (IOException ex)
+			{
+				Debug.WriteLine ("loadConfFile Error: " + ex.Message);
+			}
+			return data;
+		}
+
+		/// <summary>
+		/// Saves the config to file path.
+		/// Debugs to console if Error.
+		/// </summary>
+		/// <returns><c>True</c>, if config file was saved, <c>false</c> otherwise.</returns>
+		/// <param name="filePath">Path and Name to the file to be saved.</param>
+		/// <param name="data">Data to be saved (should be JSON)</param>
+		private bool saveConfFile(string filePath, string data)
+		{
+			bool isSaved = false; // Ponerlo a false
+			Debug.WriteLine("Saving to config file.");
+			try {
+				
+				using (StreamWriter sW = new StreamWriter(filePath))
+				{
+					sW.WriteLine(data);
+				}
+				isSaved = true;
+
+			} 
+			catch (IOException ex)
+			{
+				Debug.WriteLine ("saveConfFile Error: " + ex.Message);
+			}
+			return isSaved;
+		}
+
+		/// <summary>
+		/// Class that converts data to <c>String</c>
+		/// </summary>
+		/// <returns>JSON Parsed string</returns>
+		/// <param name="data">Class to be serialized in JSON</param>
+		private string dataToJson(botConfig data)
+		{
+			return JsonConvert.SerializeObject(data);
+		}
+
+		/// <summary>
+		/// Loads the config from the file path (must include filename in path).
+		/// </summary>
+		public void loadConfig()
+		{
+			this.conf = this.loadConfFile (this.conf.configPath);
+		}
+
+		/// <summary>
+		/// Saves the config from the class to the file (JSON).
+		/// </summary>
+		public void saveConfig()
+		{
+			this.saveConfFile (this.conf.configPath, dataToJson (this.conf));
+		}
+
+		#region "User Get's and Set's"
+		/// <summary>
+		/// Adds the an user to the auth list.
+		/// </summary>
+		/// <returns><c>true</c>, if user was added, <c>false</c> otherwise.</returns>
+		/// <param name="ID">UserID (int).</param>
+		/// <param name="firstName">User First Name (string).</param>
+		/// <param name="lastName">User Last Name (string).</param>
+		/// <param name="userName">User's UserName (string).</param>
+		/// <param name="isAuth">Is user authenticated (bool) ?</param>
+		/// <param name="inAuthProcess">Is user in authentication process (bool) ?</param>
+		/// <param name="banUntil">When is the user able to put password again (dateTime).</param>
+		/// <param name="num">User Number of trys (int).</param>
+		/// <param name="rol">User Role (usrRole).</param>
+		public void addUser(int ID, string firstName = "", string lastName = "", string userName = "",
+		bool isAuth = false, bool isAuthProcess = false,
+		int num = 3, usrRole rol = usrRole.Normal)
+		{
+			botUser data = new botUser ();
+			data.iD = ID;
+			data.firstName = firstName;
+			data.lastName = lastName;
+			data.userName = userName;
+			data.isAuth = isAuth;
+			data.inAuthProcess = isAuthProcess;
+			data.banUntil = DateTime.Now;
+			data.nrTry = num;
+			data.role = usrRole.Normal;
+
+			try {
+				conf.authList.Add (data);
+				Debug.WriteLine("New user added to the authList");
+			} 
+			catch (Exception ex) 
+			{
+				Debug.WriteLine ("addUser Error: " + ex.Message);
+			}
+		}
+			
+		/// <summary>
+		/// Checks if the user is already in the auth list
+		/// </summary>
+		/// <returns>The userID in auth list, -1 otherwise</returns>
+		/// <param name="ID">UserID (int).</param>
+		public int getIndexAuthList(int ID)
+		{
+			int data;
+			data = conf.authList.FindIndex (botUser => botUser.iD == ID);
+			Debug.WriteLine ("Is user auth in list: (int) " + data);
+			return data;
+		}
+
+		/// <summary>
+		/// Sets the user role.
+		/// </summary>
+		/// <param name="listIndex">List index.</param>
+		/// <param name="newRole">New role.</param>
+		public void setUserRole(int listIndex, usrRole newRole)
+		{
+			conf.authList [listIndex].role = newRole;
+		}
+
+		public void setUserTrys(int listIndex, int newNrTrys)
+		{
+			conf.authList [listIndex].nrTry = newNrTrys;
+		}
+
+		public void setUserIsAuth(int listIndex, bool newIsAuth)
+		{
+			conf.authList [listIndex].isAuth = newIsAuth;
+		}
+
+		public void setUserInAuthProcess(int listIndex, bool newInAuthProcess)
+		{
+			conf.authList [listIndex].inAuthProcess = newInAuthProcess;
+		}
+
+		public void setUserBanUntil(int listIndex, DateTime newBanUntil)
+		{
+			conf.authList [listIndex].banUntil = newBanUntil;
+		}
+
+		public string getUserFirstName(int listIndex)
+		{
+			return conf.authList [listIndex].firstName;
+		}
+
+		public string getUserLastName(int listIndex)
+		{
+			return conf.authList [listIndex].lastName;
+		}
+
+		public string getUserUserName(int listIndex)
+		{
+			return conf.authList [listIndex].userName;
+		}
+
+		/// <summary>
+		/// Gets the user trys.
+		/// </summary>
+		/// <returns>The user trys.</returns>
+		/// <param name="listIndex">Index of the auth list. You can get it with getIndexAuthList.</param>
+		public int getUserTrys(int listIndex)
+		{
+			return conf.authList[listIndex].nrTry;
+		}
+
+		/// <summary>
+		/// Check if the user is authenticated
+		/// </summary>
+		/// <returns><c>true</c>, if is user is authenticated, <c>false</c> otherwise.</returns>
+		/// <param name="listIndex">Index of the auth list. You can get it with getIndexAuthList.</param>
+		public bool getIsUserAuth(int listIndex)
+		{
+			return conf.authList [listIndex].isAuth;
+		}
+
+		/// <summary>
+		/// Check is the user is in the authentication process
+		/// </summary>
+		/// <returns><c>true</c>, if is user is in auth process, <c>false</c> otherwise.</returns>
+		/// <param name="listIndex">Index of the auth list. You can get it with getIndexAuthList.</param>
+		public bool getIsUserInAuthProcess(int listIndex)
+		{
+			return conf.authList [listIndex].inAuthProcess;
+		}
+
+		/// <summary>
+		/// Gets the time and date until the user is banned.
+		/// </summary>
+		/// <returns>Date and time until the user is banned.</returns>
+		/// <param name="listIndex">Index of the auth list. You can get it with getIndexAuthList.</param>
+		public DateTime getBanUntil(int listIndex) {
+			return conf.authList[listIndex].banUntil;
+		}
+		#endregion
+
+		#region "Config Get's and Set's"
+		/// <summary>
+		/// Sets the token.
+		/// </summary>
+		/// <returns><c>true</c>, if token was set, <c>false</c> otherwise.</returns>
+		/// <param name="token">Token.</param>
+		public bool setToken(string token)
+		{
+			bool _isSet = false;
+			if (chk.checkToken (token))
+			{
+				this.conf.token = token;
+				_isSet = true;
+			}
+			return _isSet;
+		}
+			
+		/// <summary>
+		/// Sets the PW.
+		/// </summary>
+		/// <returns><c>true</c>, if PW was set, <c>false</c> otherwise.</returns>
+		/// <param name="PW">PW.</param>
+		public bool setPW(string PW)
+		{
+			bool _isSet = false;
+			if (chk.checkPW(PW))
+			{
+				this.conf.authPW = PW;
+				_isSet = true;
+			}
+			return _isSet;
+		}
+
+		/// <summary>
+		/// Sets the file path where the config is saved.
+		/// </summary>
+		/// <param name="path">File path including filename.</param>
+		public void setFilePath(string path)
+		{
+			this.conf.configPath = path;
+			isPathSet = true;
+		}
+
+		/// <summary>
+		/// Sets the file path where the .torrent files are saved.
+		/// </summary>
+		/// <param name="path">Path to torrent dir.</param>
+		public void setTorrentPath(string path)
+		{
+			if (!Directory.Exists (path))
+			{
+				throw new Exception ("Not a valid path for the .torrent files.");
+			}
+			this.conf.torrentPath = path;
+		}
+
+		/// <summary>
+		/// Gets the token to start the bot.
+		/// </summary>
+		/// <returns>Returns a <c>string</c> that contains the bot Token</returns>
+		public string getToken() 
+		{
+			string token = "";
+
+			if (!isPathSet)
+			{
+				throw new IOException ("You have to set the path to the config file first");
+			}
+
+			if (!isConfigSet)
+			{
+				this.conf = loadConfFile (conf.configPath);
+			}
+
+			token = this.conf.token;
+			return token;
+		}
+
+		/// <summary>
+		/// Gets the Password to authenticate.
+		/// </summary>
+		/// <returns>Returns a <c>string</c> that contains the auth Password</returns>
+		public string getPW()
+		{
+			string pW = "";
+
+			if (!isPathSet)
+			{
+				throw new IOException ("You have to set the path to the config file first");
+			}
+
+			if (!isConfigSet)
+			{
+				this.conf = loadConfFile (conf.configPath);
+			}
+
+			pW = this.conf.authPW;
+			return pW;
+		}
+
+		/// <summary>
+		/// Gets the path to the config file.
+		/// </summary>
+		/// <returns>Returns a <c>string</c> that contains the path to the config file</returns>
+		public string getConfigPath()
+		{
+			string path = "";
+
+			if (!isPathSet)
+			{
+				throw new IOException ("You have to set the path to the config file first");
+			}
+
+			if (!isConfigSet)
+			{
+				this.conf = loadConfFile (conf.configPath);
+			}
+
+			path = this.conf.configPath;
+			return path;
+		}
+
+		/// <summary>
+		/// Gets the path where the .torrent files are saved.
+		/// </summary>
+		/// <returns>Returns a <c>string</c> that contains the path to the torrent files</returns>
+		public string getTorrentPath()
+		{
+			string path = "";
+
+			if (!isPathSet)
+			{
+				throw new IOException ("You have to set the path to the config file first");
+			}
+
+			if (!isConfigSet)
+			{
+				this.conf = loadConfFile (conf.configPath);
+			}
+
+			path = this.conf.torrentPath;
+			return path;
+		}
+		#endregion
+
+		/// <summary>
+		/// Class where all the app settings are going to be saved to we can serialize to JSON
+		/// </summary>
+		private class botConfig
+		{
+			public string token { get; set; }
+			public string authPW { get; set; }
+			public string configPath { get; set; }
+			public string torrentPath { get; set; }
+			public List<botUser> authList { get; set; }
+
+			public botConfig()
+			{
+				this.authList = new List<botUser>();
+			}
+		}
+	}
+
+	/// <summary>
+	/// Class that can manage Telegram Bot Users to make Permission System
+	/// </summary>
+	public class botUser
+	{
+		public int iD { get; set; }
+		public string firstName { get; set; }
+		public string lastName { get; set; }
+		public string userName { get; set; }
+		/// <summary>
+		/// Gets or sets the number of trys that the user has before he get's banned from the auth system
+		/// </summary>
+		/// <value>The number of trys</value>
+		public int nrTry { get; set; }
+		public bool inAuthProcess { get; set; }
+		public bool isAuth { get; set; }
+		public DateTime banUntil { get; set; }
+		/// <summary>
+		/// Gets or sets the user role in the bot.
+		/// </summary>
+		/// <value>The user role from enum <c>usrRole</c>.</value>
+		public usrRole role { get; set; }
+	}
+}
---
+++ botSettings/botSettingsEnums.cs
@@ -0,0 +1,35 @@
+﻿//
+//  botSettingsEnums.cs
+//
+//  Author:
+//       Big Cash Monkeys <admin@bcm.ovh>
+//
+//  Copyright (c) 2017 Big Cash Monkeys
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace botSettings.Enums
+{
+	/// <summary>
+	/// Usr role enum.
+	/// </summary>
+	/// <value>Normal, Admin</value>
+	public enum usrRole
+	{
+		Normal = 0x00000000,
+		Admin = 0x10000000
+	}
+
+
+}
